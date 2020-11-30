f4dd4f139ce3255ba9e6f7624adbbe059d0b6a79
Rahul Madhogarhia` <madhogarhiarahul@gmail.com>
2020-11-30 13:39:51 +0530

    Add files via upload

---
+++ misNum.py
@@ -0,0 +1,18 @@
+iList = [1, 3, 4, 6, 99]
+
+# ordList = iList.sort()
+refList = list(range(0, int(iList[-1]) + 1))
+
+misNum = []
+for i in range(len(refList)):
+    found = False
+    for j in range(len(iList)):
+        if iList[j] == refList[i]:
+            found = True
+            break
+    if not found:
+        misNum.append(refList[i])
+    else:
+        continue
+
+print(misNum)
