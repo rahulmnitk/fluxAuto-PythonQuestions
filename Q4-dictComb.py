4ba9ea8a17ed35b21d304ae598d1f25158048043
Rahul Madhogarhia` <madhogarhiarahul@gmail.com>
2020-11-30 18:57:50 +0530

    Update Q4-dictComb.py

--- Q4-dictComb.py
+++ Q4-dictComb.py
@@ -1,8 +1,21 @@
-x={'1':['a','b'], '2':['c','d']}
-list1 = x.get('1')
-list2 = x.get('2')
-for i in range(len(list1)):
-    for j in range(len(list2)):
-      print(list1[i]+list2[j])
+# Program to list all the combinations 
+# of the elements from a dictionary
+import itertools
+
+# Function to form pairs of elements 
+# using the itertools.product method
+def combos(Dict):
+    l = []
+    for pair in itertools.product(*[Dict[i] for i in sorted(Dict.keys())]):
+        l.append("".join(pair))
+    return l
+
+x = {'1':['a','b'], '2':['c','d']}
+comb_x = combos(x)
+print(comb_x)
+
+y = {'1':['a', 'b', 'c'], '2':['c', 'd', 'e'], '3': ['f', 'g', 'h']}
+comb_y = combos(y)
+print(comb_y)
 
 
