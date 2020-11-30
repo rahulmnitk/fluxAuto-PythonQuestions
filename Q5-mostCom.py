f7b3a283b8fdfc1167bd216970160c0ee4dd841c
Rahul Madhogarhia` <madhogarhiarahul@gmail.com>
2020-11-30 13:14:51 +0530

    Update Q5-mostCom.py

--- Q5-mostCom.py
+++ Q5-mostCom.py
@@ -1,11 +1,9 @@
 from collections import Counter
 
-words = [
-   'red', 'green', 'black', 'pink', 'black', 'white', 'black', 'eyes',
+words = ['red', 'green', 'black', 'pink', 'black', 'white', 'black', 'eyes',
    'white', 'black', 'orange', 'pink', 'pink', 'red', 'red', 'white', 'orange',
    'white', "black", 'pink', 'green', 'green', 'pink', 'green', 'pink',
-   'white', 'orange', "orange", 'red'
-]
+   'white', 'orange', "orange", 'red']
 
 counts = Counter(words)
 n = int(input("Enter number of words: "))
