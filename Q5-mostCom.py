04062eaecf990b47d92c5b081d28ecf104285d1b
Rahul Madhogarhia` <madhogarhiarahul@gmail.com>
2020-12-01 05:18:01 +0530

    Update Q5-mostCom.py

--- Q5-mostCom.py
+++ Q5-mostCom.py
@@ -1,11 +1,20 @@
+# Program to find the most common
+# N words using collections module
+
 from collections import Counter
 
-words = ['red', 'green', 'black', 'pink', 'black', 'white', 'black', 'eyes',
-   'white', 'black', 'orange', 'pink', 'pink', 'red', 'red', 'white', 'orange',
-   'white', "black", 'pink', 'green', 'green', 'pink', 'green', 'pink',
-   'white', 'orange', "orange", 'red']
+n = int(input("Enter the value of n: "))
 
-counts = Counter(words)
-n = int(input("Enter number of words: "))
-n_mostCom = counts.most_common(n)
-print(n_mostCom)
+words = ['red', 'green', 'black', 'pink', 'black', 'white', 'black', 'eyes',
+'white', 'black', 'orange', 'pink', 'pink', 'red', 'red', 'white', 'orange',
+'white', "black", 'pink', 'green', 'green', 'pink', 'green', 'pink',
+'white', 'orange', "orange", 'red']
+
+def getCommon(words, n):
+    counts = Counter(words)
+    total_count = counts.most_common()
+    if n > total_count:
+        return("Number of desired words exceeds the total word count!")
+    return("Most common {} words are: {}".format(n, counts.most_common(n)))
+
+print(getCommon(words, n))
