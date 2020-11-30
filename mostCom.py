4de7ea3c9b00537a314f1f3ec1b5a3e5edd5a871
Rahul Madhogarhia` <madhogarhiarahul@gmail.com>
2020-11-30 13:11:59 +0530

    Add files via upload

---
+++ mostCom.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+words = [
+   'red', 'green', 'black', 'pink', 'black', 'white', 'black', 'eyes',
+   'white', 'black', 'orange', 'pink', 'pink', 'red', 'red', 'white', 'orange',
+   'white', "black", 'pink', 'green', 'green', 'pink', 'green', 'pink',
+   'white', 'orange', "orange", 'red'
+]
+
+counts = Counter(words)
+n = input("Enter number of words: ")
+n_mostCom = counts.most_common(n)
+print(n_mostCom)
